2960fb66ded6e44a8508b101994b4dd8fa64e57c
Melisa <yensai5@hotmail.com>
2020-08-28 19:43:33 +0200

    Include changes in components of IBEX35 to generate final datasets with metrics

--- 01_Generate_historic_data.py
+++ 01_Generate_historic_data.py
@@ -57,19 +57,22 @@ def get_historic_data(component, filter_year, logger):
         final_historic_data["vol"] = final_historic_data["vol"].replace("-", np.NaN)
         # Necesitamos convertir la columna Fecha a Date
         final_historic_data['fecha'] =  pd.to_datetime(final_historic_data['fecha'], format = "%d.%m.%Y", errors ='coerce')
-        # Añadimos 2 nuevas columnas: 
+        
+        # Añadimos 3 nuevas columnas: 
         # - trimestre_anio
         # - mes_anio
+        # - anio
         final_historic_data['trimestre_anio'] = final_historic_data['fecha'].dt.quarter.map(str) + "_" + final_historic_data['fecha'].dt.year.map(str)
         final_historic_data['mes_anio'] = final_historic_data['fecha'].dt.month.map(str) + "_" + final_historic_data['fecha'].dt.year.map(str)
-        
+        final_historic_data['anio'] = final_historic_data['fecha'].dt.year.map(str)
+
         # Filtramos los datos para quedarnos con aquellos a partir del 12/02/2015 
         # Esto viene marcado por el dataset de AENA que sólo tiene datos a partir de esa fecha
         if(filter_year == True):
             final_historic_data = final_historic_data[final_historic_data["fecha"]>='2015-02-12']
         final_historic_data = final_historic_data.sort_values(by=('fecha'),ascending=True)
         
-        final_historic_data_df = pd.DataFrame(final_historic_data, columns=["fecha","ultimo","apertura","maximo","minimo","vol","variacion","trimestre_anio", "mes_anio"])
+        final_historic_data_df = pd.DataFrame(final_historic_data, columns=["fecha","ultimo","apertura","maximo","minimo","vol","variacion","trimestre_anio", "mes_anio", "anio"])
         return final_historic_data_df
 
 def get_historic_data_script(component, logger):
@@ -90,14 +93,16 @@ def get_historic_data_script(component, logger):
         final_historic_data_script["vol"] = final_historic_data_script["vol"].replace("-", np.NaN)
         # Necesitamos convertir la columna Fecha a Date
         final_historic_data_script['fecha'] =  pd.to_datetime(final_historic_data_script['fecha'], format = "%Y-%m-%d", errors ='coerce')
-        # Añadimos 2 nuevas columnas: 
+        # Añadimos 3 nuevas columnas: 
         # - trimestre_anio
         # - mes_anio
+        # - anio
         final_historic_data_script['trimestre_anio'] = final_historic_data_script['fecha'].dt.quarter.map(str) + "_" + final_historic_data_script['fecha'].dt.year.map(str)
         final_historic_data_script['mes_anio'] = final_historic_data_script['fecha'].dt.month.map(str) + "_" + final_historic_data_script['fecha'].dt.year.map(str)
+        final_historic_data_script['anio'] = final_historic_data_script['fecha'].dt.year.map(str)
         final_historic_data_script = final_historic_data_script.sort_values(by=('fecha'),ascending=True)
         
-        final_historic_data_script_df = pd.DataFrame(final_historic_data_script, columns=["fecha","ultimo","apertura","maximo","minimo","vol","variacion","trimestre_anio", "mes_anio"])
+        final_historic_data_script_df = pd.DataFrame(final_historic_data_script, columns=["fecha","ultimo","apertura","maximo","minimo","vol","variacion","trimestre_anio", "mes_anio", "anio"])
         return final_historic_data_script_df
     
     
@@ -166,6 +171,68 @@ def add_pib(component, final_historic_data, logger):
     pib_df = pd.DataFrame(pib, columns=["pib", "trimestre_anio"])
     final_historic_data_with_pib = final_historic_data.merge(pib_df, on="trimestre_anio", how="left")
     return final_historic_data_with_pib
+
+def add_metrics(component, final_historic_data, logger):
+    logger.info('-- add_metrics > component:' + component)
+    
+    # Cargamos las métricas por defecto
+    years_default_metrics = ["2017", "2018", "2019", "2020", "2021"]
+    all_default_metrics_data = []
+    for year in years_default_metrics:
+        metric_filename = './dataset/' + component + '/default_metrics_' + component + '_'+year+'.csv'
+        if os.path.exists(metric_filename):
+            default_metric = pd.read_csv(metric_filename, index_col=None, header=0)
+            default_metric['anio'] = year
+            all_default_metrics_data.append(default_metric)
+
+    final_default_metrics = pd.concat(all_default_metrics_data, axis=0, ignore_index=True)
+    final_default_metrics = final_default_metrics.set_index('anio')
+    
+    # Cargamos las métricas obtenidas hasta el momento para ese componente
+    all_metrics_data = []
+    files = glob.glob(r'./dataset/' + component + '/metrics_' + component + '_*.csv')
+    if(len(files)>0):
+        for file in files:
+            metric = pd.read_csv(file, index_col=None, header=0)
+            all_metrics_data.append(metric)
+    
+        final_metrics_data = pd.concat(all_metrics_data, axis=0, ignore_index=True)
+        # eliminamos las columnas que no nos hacen falta
+        final_metrics_data = final_metrics_data.drop(['precio_div_cash_flow', 'b_neto'], axis=1)
+        final_metrics_data['fecha'] =  pd.to_datetime(final_metrics_data['fecha'], format = "%Y-%m-%d", errors ='coerce')
+    
+    # Hacemos merge del dataframe final_historic_data con el dataframe final_metrics_data para añadir las nuevas
+    # columnas per, bpa, rentabilidad_por_dividendo, precio_valor_contable y ebitda
+    # Estos valores son los que se han ido obteniendo vía webscraping
+    final_historic_data_with_metrics = final_historic_data.merge(final_metrics_data, on="fecha", how="left")
+    
+    # Como disponemos de valores por defecto para las métricas desde el año 2017 vamos asignar los correspondientes
+    # valores por defecto si el campo no tiene valor
+    final_historic_data_with_metrics[component + '_per'] = final_historic_data_with_metrics.apply(
+            lambda row: 
+                (final_default_metrics.loc[row['anio']]['per'] if row['anio'] in final_default_metrics.index else row['per']) if pd.isna(row['per']) else row['per'], 
+                axis=1)
+    final_historic_data_with_metrics[component + '_bpa'] = final_historic_data_with_metrics.apply(
+            lambda row: 
+                (final_default_metrics.loc[row['anio']]['bpa'] if row['anio'] in final_default_metrics.index else row['bpa']) if pd.isna(row['bpa']) else row['bpa'], 
+                axis=1)    
+    final_historic_data_with_metrics[component + '_rentabilidad_por_dividendo'] = final_historic_data_with_metrics.apply(
+            lambda row: 
+                (final_default_metrics.loc[row['anio']]['rentabilidad_por_dividendo'] if row['anio'] in final_default_metrics.index else row['rentabilidad_por_dividendo']) if pd.isna(row['rentabilidad_por_dividendo']) else row['rentabilidad_por_dividendo'], 
+                axis=1) 
+    final_historic_data_with_metrics[component + '_precio_valor_contable'] = final_historic_data_with_metrics.apply(
+            lambda row: 
+                (final_default_metrics.loc[row['anio']]['precio_valor_contable'] if row['anio'] in final_default_metrics.index else row['precio_valor_contable']) if pd.isna(row['precio_valor_contable']) else row['precio_valor_contable'], 
+                axis=1) 
+    final_historic_data_with_metrics[component + '_ebitda'] = final_historic_data_with_metrics.apply(
+            lambda row: 
+                (final_default_metrics.loc[row['anio']]['ebitda'] if row['anio'] in final_default_metrics.index else row['ebitda']) if pd.isna(row['ebitda']) else row['ebitda'], 
+                axis=1) 
+    # Eliminamos las columnas que no nos hacen falta
+    final_historic_data_with_metrics = final_historic_data_with_metrics.drop(['apertura','maximo','minimo','vol','variacion','per','bpa','rentabilidad_por_dividendo','precio_valor_contable','ebitda'], axis=1)
+    # Rename de la columna ultimo a ITEM_ultimo
+    final_historic_data_with_metrics.rename(columns = {'ultimo':component+'_ultimo'}, inplace = True)
+    return final_historic_data_with_metrics
     
     
 def save_historic_data(component, columns, final_historic_data, filter_year, logger):
@@ -189,7 +256,7 @@ def save_historic_data(component, columns, final_historic_data, filter_year, log
 def main():
     logger = set_timed_rotating_log('logs/historic_data.log')
     components = ['IBEX35','AENA','AMS','BBVA','CABK','FER','IBE','ITX','REP','SAN','TEF']
-    filter_year = False
+    filter_year = True
 
     logger.info('Inicio generate historic data')
     for item in components:
@@ -198,19 +265,25 @@ def main():
         # Juntamos los dos dataframes
         final_historic_data_concat = pd.concat([final_historic_data, final_historic_data_script], axis=0, ignore_index=True)
        
-        final_historic_data_with_dp = add_deuda_publica(item, final_historic_data_concat, logger)
-        final_historic_data_with_ipc = add_ipc(item, final_historic_data_with_dp, logger)
-        final_historic_data_with_tasa_paro = add_tasa_paro(item, final_historic_data_with_ipc, logger)
-        final_historic_data_with_pib = add_pib(item, final_historic_data_with_tasa_paro, logger)
+        if(item=='IBEX35'):
+            final_historic_data_with_dp = add_deuda_publica(item, final_historic_data_concat, logger)
+            final_historic_data_with_ipc = add_ipc(item, final_historic_data_with_dp, logger)
+            final_historic_data_with_tasa_paro = add_tasa_paro(item, final_historic_data_with_ipc, logger)
+            final_historic_data_with_pib = add_pib(item, final_historic_data_with_tasa_paro, logger)
+            final_historic_data_with_metrics = final_historic_data_with_pib
+            columns_historic_data = ["fecha","ultimo","apertura","maximo","minimo","vol","variacion","deuda_publica","ipc","tasa_paro","pib"]
+        else:
+            final_historic_data_with_metrics = add_metrics(item, final_historic_data_concat, logger)
+            columns_historic_data = ["fecha",item+ "_ultimo",item+"_per",item+"_bpa",item+"_rentabilidad_por_dividendo",item+"_precio_valor_contable",item+"_ebitda"]
+
         
         # Eliminamos los valores duplicados
-        final_historic_data_with_pib.drop_duplicates(subset="fecha", keep="last", inplace=True)
-        # Eliminamos las columnas "trimestre_anio" y "mes_anio"
-        columns_drop = ["trimestre_anio", "mes_anio"]
-        final_historic_data_with_pib = final_historic_data_with_pib.drop(columns_drop, axis=1)
+        final_historic_data_with_metrics.drop_duplicates(subset="fecha", keep="last", inplace=True)
+        # Eliminamos las columnas "trimestre_anio", "mes_anio" y "anio"
+        columns_drop = ["trimestre_anio", "mes_anio", "anio"]
+        final_historic_data_with_metrics = final_historic_data_with_metrics.drop(columns_drop, axis=1)
         
-        columns_historic_data = ["fecha","ultimo","apertura","maximo","minimo","vol","variacion","deuda_publica","ipc","tasa_paro","pib"]
-        save_historic_data(item, columns_historic_data, final_historic_data_with_pib, filter_year, logger)
+        save_historic_data(item, columns_historic_data, final_historic_data_with_metrics, filter_year, logger)
         
 
 if __name__ == "__main__":
